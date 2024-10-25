3ad5ca33ee082026877c8bc979117dbfc9b39e71
choiminu <alsdn4590@gmail.com>
2024-10-25 16:00:58 +0900

    JXS 문법을 사용하여 간단한 React 컴포넌트 추가

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './component/Header';
-import Sidebar from './component/Sidebar';
-import FeaturedSection from './component/FeaturedSection';
-import Footer from './component/Footer';
-import RandomNumber from './state/RandomNumber';
+import Clock from './component/Clock';
 
 function App() {
   return (
     <div className="App">
-      <RandomNumber/>
+      <Clock />
     </div>
   );
 }
---
+++ react/my-app/src/component/Clock.jsx
@@ -0,0 +1,33 @@
+import React, { useState} from "react";
+
+class Clock extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>현재 시간 : {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        )
+    };
+}
+
+export default Clock;
