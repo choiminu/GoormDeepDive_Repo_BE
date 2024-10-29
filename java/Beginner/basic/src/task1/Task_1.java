41903b973dbc46043b596c97f293ff9b86e344a7
choiminu <alsdn4590@gmail.com>
2024-10-29 17:07:29 +0900

    feat(java) : 자신의 이름과 나이를 출력하는 Java 프로그램 작성하기

---
+++ java/Beginner/basic/src/task1/Task_1.java
@@ -0,0 +1,7 @@
+package task1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요 저는 최민우이고, 나이는 25살입니다.");
+    }
+}
--- java/goorm/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(add(5, 10));
-    }
-
-    public static int add(int value1, int value2) {
-        return value1 + value2;
-    }
-}
