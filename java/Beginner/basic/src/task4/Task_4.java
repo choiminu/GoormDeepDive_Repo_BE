c75dd2bf070058431e6520e1867b261f937cc149
choiminu <alsdn4590@gmail.com>
2024-10-29 17:13:01 +0900

    feat (java): 5개의 정수 오름차순 정렬하기

---
+++ java/Beginner/basic/src/task4/Task_4.java
@@ -0,0 +1,13 @@
+package task4;
+
+import java.util.Arrays;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        int[] arr = new int[] {5,3,1,2,4};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
