73c7e3a5dd0146f74387534efcf61506eb3583dd
choiminu <alsdn4590@gmail.com>
2024-10-29 17:11:21 +0900

    feat (java): 1부터 10까지의 숫자 배열 출력하기

---
+++ java/Beginner/basic/src/task3/Task_3.java
@@ -0,0 +1,9 @@
+package task3;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
