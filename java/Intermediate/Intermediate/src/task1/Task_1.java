cb4ffaef2e2b5dd81178e60c4a7ece894d977c2f
choiminu <alsdn4590@gmail.com>
2024-10-29 23:45:32 +0900

    feat(java): 이름과 나이를 입력받아 출력하는 프로그램 작성하기

---
+++ java/Intermediate/Intermediate/src/task1/Task_1.java
@@ -0,0 +1,19 @@
+package task1;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("이름을 입력해주세요 : ");
+        String name = sc.nextLine();
+
+        System.out.println();
+
+        System.out.print("나이를 입력해주세요 : ");
+        int age = sc.nextInt();
+
+        System.out.println("이름 = " + name + " 나이 " + age);
+    }
+}
