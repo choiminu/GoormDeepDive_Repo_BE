02747c0758e2f546d9f63cc13ffea826b3cbc9a2
choiminu <alsdn4590@gmail.com>
2024-10-31 14:50:43 +0900

    feat (spring, my-board) : 싱글톤 빈 스코프와 프로토타입 빈 스코프 구현하기

--- spring/my-board/src/main/java/com/board/myboard/MyBoardApplication.java
+++ spring/my-board/src/main/java/com/board/myboard/MyBoardApplication.java
@@ -2,6 +2,8 @@ package com.board.myboard;
 
 import com.board.myboard.bean.ExpController;
 import com.board.myboard.bean.ExpService;
+import com.board.myboard.bean.scope.ProtoTypeBean;
+import com.board.myboard.bean.scope.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,9 +17,13 @@ public class MyBoardApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(MyBoardApplication.class, args);
 
-        ExpController expController = applicationContext.getBean("expController", ExpController.class);
+        ProtoTypeBean protoBean = applicationContext.getBean("protoTypeBean", ProtoTypeBean.class);
+        System.out.println("protoBean = " + protoBean);
 
-        expController.call();
+        SingletonBean singletonBean = applicationContext.getBean("singletonBean", SingletonBean.class);
+        System.out.println("singletonBean = " + singletonBean);
 
+//        ExpController expController = applicationContext.getBean("expController", ExpController.class);
+//        expController.call();
     }
 }
---
+++ spring/my-board/src/main/java/com/board/myboard/bean/scope/ProtoTypeBean.java
@@ -0,0 +1,21 @@
+package com.board.myboard.bean.scope;
+
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "prototype")
+@Slf4j
+public class ProtoTypeBean {
+
+    public ProtoTypeBean() {
+        log.info("ProtoTypeBean 객체가 생성됨.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.info("ProtoTypeBean 객체가 소멸됨.");
+    }
+}
