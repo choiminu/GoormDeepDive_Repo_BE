dcb0742de5ba9f10cbb6586dc59cca42acf8a575
choiminu <alsdn4590@gmail.com>
2024-11-20 17:45:55 +0900

    feat (spring, dbexlorer) : 프로젝트 생성 및 초기화

---
+++ Spring/DBExplorer/src/main/java/com/dbexplorer/DbExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.dbexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbExplorerApplication.class, args);
+    }
+
+}
