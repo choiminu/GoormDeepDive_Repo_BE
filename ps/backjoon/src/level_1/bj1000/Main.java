687f346fe9b82a34daf0ec52364341c42a6fd13f
choiminu <alsdn4590@gmail.com>
2024-12-03 22:25:52 +0900

    feat (ps) : 백준 1000 문제 해결

---
+++ ps/backjoon/src/level_1/bj1000/Main.java
@@ -0,0 +1,10 @@
+package level_1.bj1000;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
