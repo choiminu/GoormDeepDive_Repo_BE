99335898e5ec546d8e7979fe945aa9f5b1778c55
choiminu <alsdn4590@gmail.com>
2024-12-03 22:42:41 +0900

    feat (ps) : 백준 10818 문제 해결

---
+++ ps/backjoon/src/level_1/bj10818/Main.java
@@ -0,0 +1,9 @@
+package level_1.bj10818;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).nextInt() - 543);
+    }
+}
