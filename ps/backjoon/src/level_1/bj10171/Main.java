2856e366d22d9c660633dd2af716884347028ed3
choiminu <alsdn4590@gmail.com>
2024-12-03 22:57:24 +0900

    feat (ps) : 백준 10172 문제 해결

---
+++ ps/backjoon/src/level_1/bj10171/Main.java
@@ -0,0 +1,11 @@
+package level_1.bj10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n"
+                + "|q p|   /}\n"
+                + "( 0 )\"\"\"\\\n"
+                + "|\"^\"`    |\n"
+                + "||_/=\\\\__|\n");
+    }
+}
