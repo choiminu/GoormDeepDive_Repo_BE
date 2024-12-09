50d2633e05a43b8a16c298bc5d8cebc2c6b53f69
choiminu <alsdn4590@gmail.com>
2024-12-10 08:33:12 +0900

    feat (ps) : 백준 8393 문제 해결

---
+++ ps/backjoon/src/level_3/bj8393/Main.java
@@ -0,0 +1,12 @@
+package level_3.bj8393;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n * (n+1) / 2);
+    }
+}
