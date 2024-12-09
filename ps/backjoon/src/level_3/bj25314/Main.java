ee809bfe8e31fd48b3f262cc2f7e5b96ac01030c
choiminu <alsdn4590@gmail.com>
2024-12-10 08:43:34 +0900

    feat (ps) : 백준 25314 문제 해결

---
+++ ps/backjoon/src/level_3/bj25314/Main.java
@@ -0,0 +1,12 @@
+package level_3.bj25314;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("long ".repeat(n / 4) + "int");
+    }
+}
